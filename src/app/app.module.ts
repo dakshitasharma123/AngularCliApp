d77c986fd689a3e2379028bae4b4200b3076b2fa
manishk26 <manish_m160043ca@nitc.ac.in>
2018-06-26 19:50:03 +0530

    deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,42 +4,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import {HeaderComponent} from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { TestComponent } from './test/test.component';
-import { SubTestComponent } from './test/sub-test/sub-test.component';
-import { Test1Component } from './test1/test1.component';
-import { CockpitComponent } from './test1/cockpit/cockpit.component';
-import { ServerElementComponent } from './test1/server-element/server-element.component';
-import { LifecycleHooksComponent } from './test/lifecycle-hooks/lifecycle-hooks.component';
 
 
 @NgModule({
-  declarations: [AppComponent,
-  HeaderComponent,
-   RecipesComponent,
-    RecipeListComponent,
-     RecipeDetailComponent,
-      RecipeItemComponent,
-       ShoppingListComponent,
-        ShoppingEditComponent,
-         TestComponent,
-          SubTestComponent,
-           Test1Component,
-            CockpitComponent,
-             ServerElementComponent,
-             LifecycleHooksComponent
-             ],
+  declarations: [AppComponent],
   imports: [
   	BrowserModule,
   	 FormsModule
   ],
   providers: [],
-  bootstrap: [TestComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
