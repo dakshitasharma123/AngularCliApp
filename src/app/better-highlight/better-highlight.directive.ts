53e03ffc586cc12bdbdf0b903f6313c328cad55f
manishk26 <manish_m160043ca@nitc.ac.in>
2018-06-27 19:07:29 +0530

    modifying app.component.html

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,24 +1,26 @@
-import { Directive,Renderer2,ElementRef,OnInit, HostListener,HostBinding } from '@angular/core';
+import { Directive,Renderer2,ElementRef,OnInit, HostListener,HostBinding ,Input} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-	@HostBinding('style.backgroundColor') backgroundcolor:string="green";
+	@Input() defaultColor:string="transparent";
+	@Input() highlighedColor:string="blue";
+	@HostBinding('style.backgroundColor') backgroundcolor:string;
 
   constructor(private elementRef:ElementRef, private randerer:Renderer2) { }
 
   ngOnInit(){
-  	this.randerer.setStyle(this.elementRef.nativeElement,'backgroundColor','blue');
-  	this.randerer.setStyle(this.elementRef.nativeElement,'color','white');
+  	//this.randerer.setStyle(this.elementRef.nativeElement,'backgroundColor','blue');
+  	this.backgroundcolor=this.defaultColor;
   }
   @HostListener('mouseenter') mouseover(event:Event){
   	//this.randerer.setStyle(this.elementRef.nativeElement,'font-size','50px');
-  	this.backgroundcolor="orange";//use this if you use @HostBiding
+  	this.backgroundcolor=this.highlighedColor;//use this if you use @HostBiding
   }
     @HostListener('mouseleave') mouseleave(event:Event){
   	/*this.randerer.setStyle(this.elementRef.nativeElement,'backgroundColor','blue');
   	this.randerer.setStyle(this.elementRef.nativeElement,'font-size','16px');*/
-  	this.backgroundcolor="blue";//use this if you use @HostBiding
+  	this.backgroundcolor=this.defaultColor;//use this if you use @HostBiding
   }
 }
