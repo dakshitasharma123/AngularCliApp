e676aafa717795b74ba5c9c0ff9af8428e7600fd
manishk26 <manish_m160043ca@nitc.ac.in>
2018-07-04 17:49:25 +0530

    implementing routing and navigations in app project

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,13 +9,11 @@ import {RecipeService} from '../../recipe.service';
 })
 export class RecipeItemComponent implements OnInit {
 	@Input() recipe:Recipe;
+  @Input() index:number;
 
-  constructor(private recipeservice:RecipeService) { }
+  constructor() { }
 
   ngOnInit() { 
-  }
-  onSelected(){
-    this.recipeservice.recipeSelected.emit(this.recipe);
   }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router,ActivatedRoute} from '@angular/router';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,9 +11,16 @@ import {RecipeService} from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
 	recipes:Recipe[];
- constructor(private recipeservice:RecipeService) { }
+ constructor(private recipeservice:RecipeService,
+ 			private router:Router,
+ 			private route:ActivatedRoute
+ 			) { }
 
   ngOnInit() {
   	this.recipes=this.recipeservice.getRecipes();
+  }
+  onNewRecipe()
+  {
+  	this.router.navigate(['new'], {relativeTo: this.route});
   }
 }
