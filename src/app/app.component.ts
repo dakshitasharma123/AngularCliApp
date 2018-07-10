f883bf47eeb815b80dfca4d6a2a21e763949e8e9
manishk26 <manish_m160043ca@nitc.ac.in>
2018-07-11 02:30:15 +0530

    example of http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,68 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	 appStatus=new Promise((resolve,reject)=>{
-		setInterval(()=>{
-			resolve('Online');
-		},2000);
-	});
-
   servers = [
     {
-      instanceType: 'medium',
-      name: 'Production Server',
-      status: 'stable',
-      started: new Date(15, 1, 2017)
+      name: 'Testserver',
+      capacity: 10,
+      id: this.generateId()
     },
     {
-      instanceType: 'large',
-      name: 'User Database',
-      status: 'stable',
-      started: new Date(15, 1, 2017)
-    },
-    {
-      instanceType: 'small',
-      name: 'Development Server',
-      status: 'offline',
-      started: new Date(15, 1, 2017)
-    },
-    {
-      instanceType: 'small',
-      name: 'Testing Environment Server',
-      status: 'stable',
-      started: new Date(15, 1, 2017)
+      name: 'Liveserver',
+      capacity: 100,
+      id: this.generateId()
     }
   ];
-  filterstatus:string='';
-  onAddServer(){
-  	this.servers.push({
-  		instanceType:'big',
-  		name:'new Server',
-  		status:'online',
-  		started:new Date(8,8,2018)
-  	});
+  onAddServer(name: string) {
+    this.servers.push({
+      name: name,
+      capacity: 50,
+      id: this.generateId()
+    });
   }
-/*  onInput()
-  {
-  	let variable:boolean;
-  	for(const server of this.servers)
-  	{
-  		if(server.status===this.filterstatus)
-  			{
-  				this.numberOfServer++;
-  				variable=true;
-  			}
-  	}
-  	if(variable)
-  	console.log(this.numberOfServer);
-  variable=false;
-  	this.numberOfServer=0;
-  }*/
-  getStatusClasses(server: {instanceType: string, name: string, status: string, started: Date}) {
-    return {
-      'list-group-item-success': server.status === 'stable',
-      'list-group-item-warning': server.status === 'offline',
-      'list-group-item-danger': server.status === 'critical'
-    };
+  private generateId() {
+    return Math.round(Math.random() * 10000);
   }
 }
